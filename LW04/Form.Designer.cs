c5669f3a1e64534605a7b1958ccc150bff6b53ae
dasha <amayashimizu89@gmail.com>
2022-11-06 15:53:41 +0400

    create, copy and delete

--- LW04/Form.Designer.cs
+++ LW04/Form.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.buttonCopyFile = new System.Windows.Forms.Button();
+            this.buttonCopyCatalog = new System.Windows.Forms.Button();
+            this.buttonDeleteFile = new System.Windows.Forms.Button();
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.labelFile = new System.Windows.Forms.Label();
+            this.buttonDeleteCatalog = new System.Windows.Forms.Button();
+            this.buttonCatalog = new System.Windows.Forms.Button();
+            this.textBoxCatalog = new System.Windows.Forms.TextBox();
+            this.labelCatalog = new System.Windows.Forms.Label();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.buttonCopyFile);
+            this.groupBox.Controls.Add(this.buttonCopyCatalog);
+            this.groupBox.Controls.Add(this.buttonDeleteFile);
+            this.groupBox.Controls.Add(this.buttonFile);
+            this.groupBox.Controls.Add(this.textBoxFile);
+            this.groupBox.Controls.Add(this.labelFile);
+            this.groupBox.Controls.Add(this.buttonDeleteCatalog);
+            this.groupBox.Controls.Add(this.buttonCatalog);
+            this.groupBox.Controls.Add(this.textBoxCatalog);
+            this.groupBox.Controls.Add(this.labelCatalog);
+            this.groupBox.Controls.Add(this.treeView);
+            this.groupBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox.Location = new System.Drawing.Point(0, 0);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(200, 450);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "C:/";
+            // 
+            // buttonCopyFile
+            // 
+            this.buttonCopyFile.Location = new System.Drawing.Point(7, 370);
+            this.buttonCopyFile.Name = "buttonCopyFile";
+            this.buttonCopyFile.Size = new System.Drawing.Size(181, 23);
+            this.buttonCopyFile.TabIndex = 10;
+            this.buttonCopyFile.Text = "Копировать";
+            this.buttonCopyFile.UseVisualStyleBackColor = true;
+            this.buttonCopyFile.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonCopyCatalog
+            // 
+            this.buttonCopyCatalog.Location = new System.Drawing.Point(7, 268);
+            this.buttonCopyCatalog.Name = "buttonCopyCatalog";
+            this.buttonCopyCatalog.Size = new System.Drawing.Size(181, 23);
+            this.buttonCopyCatalog.TabIndex = 9;
+            this.buttonCopyCatalog.Text = "Копировать";
+            this.buttonCopyCatalog.UseVisualStyleBackColor = true;
+            this.buttonCopyCatalog.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonDeleteFile
+            // 
+            this.buttonDeleteFile.Location = new System.Drawing.Point(7, 341);
+            this.buttonDeleteFile.Name = "buttonDeleteFile";
+            this.buttonDeleteFile.Size = new System.Drawing.Size(181, 23);
+            this.buttonDeleteFile.TabIndex = 8;
+            this.buttonDeleteFile.Text = "Удалить";
+            this.buttonDeleteFile.UseVisualStyleBackColor = true;
+            this.buttonDeleteFile.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Location = new System.Drawing.Point(113, 312);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonFile.TabIndex = 7;
+            this.buttonFile.Text = "Добавить";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            this.buttonFile.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(7, 312);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(100, 23);
+            this.textBoxFile.TabIndex = 6;
+            // 
+            // labelFile
+            // 
+            this.labelFile.AutoSize = true;
+            this.labelFile.Location = new System.Drawing.Point(7, 294);
+            this.labelFile.Name = "labelFile";
+            this.labelFile.Size = new System.Drawing.Size(36, 15);
+            this.labelFile.TabIndex = 5;
+            this.labelFile.Text = "Файл";
+            // 
+            // buttonDeleteCatalog
+            // 
+            this.buttonDeleteCatalog.Location = new System.Drawing.Point(7, 241);
+            this.buttonDeleteCatalog.Name = "buttonDeleteCatalog";
+            this.buttonDeleteCatalog.Size = new System.Drawing.Size(181, 23);
+            this.buttonDeleteCatalog.TabIndex = 4;
+            this.buttonDeleteCatalog.Text = "Удалить";
+            this.buttonDeleteCatalog.UseVisualStyleBackColor = true;
+            this.buttonDeleteCatalog.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonCatalog
+            // 
+            this.buttonCatalog.Location = new System.Drawing.Point(113, 212);
+            this.buttonCatalog.Name = "buttonCatalog";
+            this.buttonCatalog.Size = new System.Drawing.Size(75, 23);
+            this.buttonCatalog.TabIndex = 3;
+            this.buttonCatalog.Text = "Добавить";
+            this.buttonCatalog.UseVisualStyleBackColor = true;
+            this.buttonCatalog.Click += new System.EventHandler(this.buttonCatalog_Click);
+            // 
+            // textBoxCatalog
+            // 
+            this.textBoxCatalog.Location = new System.Drawing.Point(7, 212);
+            this.textBoxCatalog.Name = "textBoxCatalog";
+            this.textBoxCatalog.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCatalog.TabIndex = 2;
+            // 
+            // labelCatalog
+            // 
+            this.labelCatalog.AutoSize = true;
+            this.labelCatalog.Location = new System.Drawing.Point(7, 194);
+            this.labelCatalog.Name = "labelCatalog";
+            this.labelCatalog.Size = new System.Drawing.Size(50, 15);
+            this.labelCatalog.TabIndex = 1;
+            this.labelCatalog.Text = "Каталог";
+            // 
+            // treeView
+            // 
+            this.treeView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.treeView.Location = new System.Drawing.Point(3, 19);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(194, 173);
+            this.treeView.TabIndex = 0;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(200, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(600, 450);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.groupBox);
+            this.Name = "Form";
+            this.Text = "File system";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private GroupBox groupBox;
+        private Button buttonDeleteFile;
+        private Button buttonFile;
+        private TextBox textBoxFile;
+        private Label labelFile;
+        private Button buttonDeleteCatalog;
+        private Button buttonCatalog;
+        private TextBox textBoxCatalog;
+        private Label labelCatalog;
+        private TreeView treeView;
+        private PictureBox pictureBox;
+        private Button buttonCopyFile;
+        private Button buttonCopyCatalog;
     }
 }
--- LW04/Form.cs
+++ LW04/Form.cs
@@ -1,10 +1,455 @@
+using System.Collections;
+
 namespace LW04
 {
     public partial class Form : System.Windows.Forms.Form
     {
+        private static Random random = new();
+        // заполненные ячейки. 1 - заполнен, 0 - свободен
+        private static int[] freeElems;
+        // максимальный размер
+        private static int maxSizeFS = 128;
+        // текущий размер
+        private int currCapacity;
+        private Bitmap bmp;
+        // размеры клеток при отрисовке
+        int xSize = 40;
+        int ySize = 40;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
         public Form()
         {
             InitializeComponent();
+            freeElems = new int[maxSizeFS];
+            currCapacity = 0;
+            bmp = new(pictureBox.Width, pictureBox.Height);
+            treeView.BeginUpdate();
+        }
+
+        // кнопки
+
+        /// <summary>
+        /// Создание файла
+        /// </summary>
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxFile.Text))
+            {
+                MessageBox.Show("Введите имя", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (treeView.SelectedNode is TreeNode)
+            {
+                if (treeView.SelectedNode.Text.Contains(".catalog"))
+                {
+                    int size = random.Next(1, 10);  //размер файла
+                    currCapacity += size;           //текущая вместимость ФС увеличилась
+                    if (currCapacity > maxSizeFS)
+                    {
+                        currCapacity -= size;
+                        size = maxSizeFS - currCapacity;
+                        currCapacity += size;
+                    }
+                    if (currCapacity >= maxSizeFS & size == 0)
+                    {
+                        MessageBox.Show("Переполнение файловой системы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    File file = new File(textBoxFile.Text + ".file", size); //size
+
+                    treeView.SelectedNode.Nodes.Add(file);
+                }
+            }
+            else return;
+            treeView.EndUpdate();
+            DrawStart();
+            pictureBox.Image = bmp;
+        }
+        /// <summary>
+        /// Создание каталога
+        /// </summary>
+        private void buttonCatalog_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxCatalog.Text))
+            {
+                MessageBox.Show("Введите имя", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (treeView.SelectedNode == null)
+                treeView.Nodes.Add(new Catalog(textBoxCatalog.Text + ".catalog"));
+            else if (treeView.SelectedNode.Text.Contains(".catalog"))
+            {
+                treeView.SelectedNode.Nodes.Add(new Catalog(textBoxCatalog.Text + ".catalog"));
+            }
+            DrawStart();
+            treeView.EndUpdate();
+        }
+        /// <summary>
+        /// Выделение выбранного TreeNode
+        /// </summary>
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            DrawStart();
+            if (treeView.SelectedNode is File)
+            {
+                File file = (File)treeView.SelectedNode;
+                Draw(file.iNode);
+            }
+            else if (treeView.SelectedNode is Catalog)
+            {
+                CycleCatalog((Catalog)treeView.SelectedNode);
+            }
+            pictureBox.Image = bmp;
+        }
+        /// <summary>
+        /// Удаление файла/каталога
+        /// </summary>
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode is File)
+            {
+                DeleteFile((File)treeView.SelectedNode);
+            }
+            else if (treeView.SelectedNode is Catalog)
+            {
+                DeleteCatalog((Catalog)treeView.SelectedNode);
+            }
+
+            if (treeView.SelectedNode != null)
+                treeView.Nodes.Remove(treeView.SelectedNode);
+            DrawStart();
+        }
+        /// <summary>
+        /// Копирование файла/каталога
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            if (treeView.SelectedNode is File)
+            {
+                CopyFile(treeView.SelectedNode as File, treeView.SelectedNode.Parent);
+            }
+            else if (treeView.SelectedNode is Catalog)
+            {
+                TreeNode parent = treeView.SelectedNode.Parent;
+                if (currCapacity + CatalogSize(treeView.SelectedNode as Catalog) > maxSizeFS)
+                {
+                    MessageBox.Show("Переполнение файловой системы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                else
+                {
+                    CopyCatalog((Catalog)treeView.SelectedNode, parent);
+                }
+            }
+        }
+        
+        // вспомогательные методы (логика)
+
+        /// <summary>
+        /// Копирование каталога
+        /// </summary>
+        /// <param name="catalog">Копируемый каталог</param>
+        /// <param name="parent">Родитель каталога</param>
+        private void CopyCatalog(Catalog catalog, TreeNode parent)
+        {
+            if (parent == null)
+                treeView.Nodes.Add(new Catalog(catalog.Text + ".copy"));
+            else
+                parent.Nodes.Add(new Catalog(catalog.Text + ".copy"));
+
+            foreach (TreeNode tn in catalog)
+            {
+                if (tn is File)
+                {
+                    if (parent == null)
+                        CopyFile(tn as File, treeView.Nodes[treeView.Nodes.Count - 1]);
+                    else
+                        CopyFile(tn as File, parent.Nodes[parent.Nodes.Count - 1]);
+                }
+                else if (tn is Catalog)
+                {
+                    if (parent == null)
+                        CopyCatalog(tn as Catalog, treeView.Nodes[treeView.Nodes.Count - 1]);
+                    else
+                        CopyCatalog(tn as Catalog, parent.Nodes[parent.Nodes.Count - 1]);
+                }
+            }
+        }
+        /// <summary>
+        /// Копирование файла
+        /// </summary>
+        /// <param name="curFile">Копируемый файл</param>
+        /// <param name="parent">Родитель файла</param>
+        private void CopyFile(File curFile, TreeNode parent)
+        {
+            if (parent.Text.Contains(".catalog"))
+            {
+                int size = 0;  //размер файла
+                iNode temp = curFile.iNode;
+                while (temp != null)
+                {
+                    size += temp.Data.Length;
+                    temp = temp.Next;
+                }
+                currCapacity += size;           //текущая вместимость ФС увеличилась
+                if (currCapacity > maxSizeFS)
+                {
+                    MessageBox.Show("Переполнение файловой системы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                File file = new File(curFile.Text + ".copy", size); //size
+
+                parent.Nodes.Add(file);
+            }
+            DrawStart();
+        }
+        /// <summary>
+        /// Удаление каталога
+        /// </summary>
+        /// <param name="catalog">Каталог</param>
+        private void DeleteCatalog(Catalog catalog)
+        {
+            foreach (TreeNode item in catalog)
+            {
+                if (item is File)
+                {
+                    DeleteFile((File)item);
+                }
+                else if (item is Catalog)
+                {
+                    DeleteCatalog(item as Catalog);
+                }
+            }
+        }
+        /// <summary>
+        /// Удаление файла
+        /// </summary>
+        /// <param name="file">Файл</param>
+        private void DeleteFile(File file)
+        {
+            iNode node = file.iNode;
+            if (node != null)
+            {
+                foreach (int index in node.Data)
+                {
+                    freeElems[index] = 0;
+                    currCapacity--;
+                }
+                while (node.Next != null)
+                {
+                    node = node.Next;
+                    foreach (int index in node.Data)
+                    {
+                        freeElems[index] = 0;
+                        currCapacity--;
+                    }
+                }
+            }
+
+        }
+        /// <summary>
+        /// Определение размера каталога
+        /// </summary>
+        /// <param name="catalog">Каталог</param>
+        private int CatalogSize(Catalog catalog)
+        {
+            int size = 0;
+            foreach (TreeNode item in catalog)
+            {
+                if (item is File)
+                {
+                    iNode temp = (item as File).iNode;
+                    while (temp != null)
+                    {
+                        size += temp.Data.Length;
+                        temp = temp.Next;
+                    }
+                }
+                else if (item is Catalog)
+                {
+                    size += CatalogSize(item as Catalog);
+                }
+            }
+            return size;
+        }
+
+        // все, что касается отрисовки
+
+        /// <summary>
+        /// Отрисовка всей ФС
+        /// </summary>
+        private void DrawStart()
+        {
+            Graphics g = Graphics.FromImage(bmp);
+
+            Brush brGr = new SolidBrush(Color.Gray);
+            Brush brBlue = new SolidBrush(Color.Blue);
+            Pen pen = new Pen(Color.Black);
+
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < freeElems.Length; i++)
+            {
+                if (freeElems[i] == 0)
+                {
+                    g.FillRectangle(brGr, x, y, xSize, ySize);
+                    g.DrawRectangle(pen, x, y, xSize, ySize);
+                    g.DrawString((i + 1).ToString(), new Font("TimesNewRoman", 12), new SolidBrush(Color.Black), new Point(x, y));
+                }
+                else if (freeElems[i] == 1)
+                {
+                    g.FillRectangle(brBlue, x, y, xSize, ySize);
+                    g.DrawRectangle(pen, x, y, xSize, ySize);
+                    g.DrawString((i + 1).ToString(), new Font("TimesNewRoman", 12), new SolidBrush(Color.Black), new Point(x, y));
+                }
+                x += xSize;
+                if (x >= pictureBox.Width)
+                {
+                    x = 0;
+                    y += ySize;
+                    if (y >= pictureBox.Height)
+                        break;
+                }
+            }
+            pictureBox.Image = bmp;
+        }
+        /// <summary>
+        /// Циклическая отрисовка каталога
+        /// </summary>
+        /// <param name="catalog">Отрисовываемый каталог</param>
+        private void CycleCatalog(Catalog catalog)
+        {
+            foreach (TreeNode item in catalog)
+            {
+                if (item is File)
+                {
+                    File file = item as File;
+                    Draw(file.iNode);
+                }
+                else if (item is Catalog)
+                {
+                    Catalog temp = item as Catalog;
+                    CycleCatalog(temp);
+                }
+            }
+        }
+        /// <summary>
+        /// Отрисовка выделенных элементов
+        /// </summary>
+        /// <param name="node">Нода, которую нужно выделить</param>
+        private void Draw(iNode node)
+        {
+            Graphics g = Graphics.FromImage(bmp);
+
+            Brush brRed = new SolidBrush(Color.Red);
+            Pen pen = new Pen(Color.Black);
+
+            int x = 0;
+            int y = 0;
+
+            for (int i = 0; i < freeElems.Length; i++)
+            {
+                if (node != null)
+                {
+                    if (node.Data.Contains(i))
+                    {
+                        g.FillRectangle(brRed, x, y, xSize, ySize);
+                        g.DrawRectangle(pen, x, y, xSize, ySize);
+                        g.DrawString((i + 1).ToString(), new Font("TimesNewRoman", 12), new SolidBrush(Color.Black), new Point(x, y));
+                    }
+                    if (node.Next != null)
+                    {
+                        Draw(node.Next);
+                    }
+                }
+                x += xSize;
+                if (x >= pictureBox.Width)
+                {
+                    x = 0;
+                    y += ySize;
+                    if (y >= pictureBox.Height)
+                        break;
+                }
+            }
+        }
+
+        // классы
+
+        /// <summary>
+        /// Каталог
+        /// </summary>
+        public class Catalog : TreeNode
+        {
+            public Catalog(string name) : base(name)
+            {
+
+            }
+
+            public IEnumerator GetEnumerator() => Nodes.GetEnumerator();
+        }
+        /// <summary>
+        /// Файл
+        /// </summary>
+        public class File : TreeNode
+        {
+            private int _size;
+            private iNode _node;
+            private string _name;
+            public int Size { get { return _size; } }
+            public iNode iNode { get { return _node; } }
+            public string Name { get { return _name; } }
+            public File(string name, int _size) : base(name)
+            {
+                _node = new iNode(_size);
+                this._size = _size;
+            }
+        }
+        /// <summary>
+        /// i-нода
+        /// </summary>
+        public class iNode
+        {
+            private int maxSize = 6;    // размер айноды
+            private int[] data;         //массив ячеек, которые файл занимает в памяти
+            public int[] Data
+            {
+                get { return data; }
+            }
+            public iNode Next { get; set; }
+            public iNode(int size)
+            {
+                if (size > maxSize)
+                {
+                    Next = new iNode(size - maxSize);
+                    size = maxSize;
+                }
+                else Next = null;
+                data = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    int temp = random.Next(maxSizeFS);  //рандомим ячейку в ФС
+                    if (freeElems[temp] == 0)
+                        freeElems[temp] = 1;
+                    else
+                    {
+                        temp = Array.IndexOf(freeElems, 0);
+                        if (temp == -1)
+                        {
+                            MessageBox.Show("Индекс вышел за границы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        freeElems[temp] = 1;
+                    }
+                    data[i] = temp;
+                }
+            }
+
         }
     }
 }
